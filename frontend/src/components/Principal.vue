7593be24ad92397ff6f78aea9847c15b44b607d3
HarrisonMalagon <harrisonleonardomalagon@live.com>
2021-10-21 18:59:36 -0500

    Update Pagina Principal

---
+++ frontend/src/components/Principal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+      <!-- Navbar content -->
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/logoP.png" alt="Logo de la pagina" width="50" height="50" class="d-inline-block align-text-center">
+            Aeroespacial Co
+        </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+      </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav ms-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Producto">Producto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Catalogo">Catalogo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Noticias</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+    <h1>Principal</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    
+};
+
+</script>
+
+<style></style>
