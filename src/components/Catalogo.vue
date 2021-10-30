68b47989a949db3f003b2294f17ba2f3047f681f
Maria Ana Lucia piñeros Zambrano <jhon89nbl@gmail.com>
2021-10-30 16:59:21 -0500

    Catalago terminado

--- src/components/Catalogo.vue
+++ src/components/Catalogo.vue
@@ -16,7 +16,7 @@
                     <a class="nav-link active" aria-current="page" href="/">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/producto">Producto</a>
+                    <a class="nav-link" href="/administrador">Producto</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/cliente">Catalogo</a>
@@ -29,15 +29,63 @@
     </div>
     </nav>
     <h1>Catalogo</h1>
+    <div id="contProductos">
+    <div  v-for="objProducto in productos" :key="objProducto._id" >
+        <div class="card" style="width: 18rem;">
+            <img id="img1" 
+            :src="objProducto.urlImagen"
+            :alt="objProducto.nombre"
+            />
+            <div class="card-body">
+            <h5 class="card-title">{{objProducto.nombre}}</h5>
+            <p class="card-text">{{ objProducto.descripcion}}</p>
+            <p class="card-text">{{ objProducto.precio}}</p>
+            
+            </div>
+        </div>
+    </div>
+    </div>
+
+
   </div>
 </template>
 
 <script>
+import store from '../../frontend/src/store'
 export default {
+    methods:{
+
+    },
+
+    created:() =>{
+        store.dispatch("cargarProductos")
+    },
+    computed:{
+        productos: () => {
+            return store.state.productos;
+        }
+    }
 
 }
 </script>
 
 <style>
+#contProductos {
+    display: grid;
+    grid-template-columns: repeat(3, 3fr);
+    grid-template-rows: 200px 75px;
+    column-gap: 20px;
+    row-gap: 50px;
+    align-content: center;
 
+}
+
+#img1{
+    width: 200px;
+    
+}
+.card{
+    align-items: center;
+    height: 350px;
+}
 </style>
